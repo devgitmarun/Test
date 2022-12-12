5092c2e6e55b80bbbb0658b8faea9de260090508
Marun <awstek786@gmail.com>
2022-12-12 21:39:49 +0530

    the first commit

---
+++ studentmanagement/src/studentmanagement/Course.java
@@ -0,0 +1,33 @@
+package studentmanagement;
+
+public class Course {
+	
+	 private int courseId;
+	 private String courseName;
+	 private int duration;
+	 private int fees;
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String  courseName) {
+		this.courseName = courseName;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public int getFees() {
+		return fees;
+	}
+	public void setFees(int fees) {
+		this.fees = fees;
+	}
+}
